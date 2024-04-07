e578adfb2d19d8bdeb106c696bf73d0abdf115f5
phuctran362003 <trangiaphuc362003181@gmail.com>
2024-04-07 17:20:31 +0700

    add executable .jar and compile dependencies

--- src/main/java/com/phuctrann/selenium/Selenium2Go.java
+++ src/main/java/com/phuctrann/selenium/Selenium2Go.java
@@ -3,7 +3,9 @@
  */
 package com.phuctrann.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
@@ -13,17 +15,19 @@ import org.openqa.selenium.edge.EdgeOptions;
  */
 public class Selenium2Go {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         searchGoogle();
     }
 
-    public static void searchGoogle() {
+    public static void searchGoogle() throws InterruptedException {
         // 1. Khai báo biến đại diện trỏ đến trình duyệt sẽ đc mở
         WebDriver myBrowser;// biến object thuộc bộ thư viện selenium
 
         //thằng này sẽ trỏ đến cái trình duyệt khi đc new
         // mỗi lần new là 1 trình duyệt đc mở ra, 1 vùng đc cấp
-        // 1 object Trình duyệt đc new trong HEAP
+        // MỘT object Trình duyệt đc new trong HEAP
+        
+        
         // 2. Khai báo người em song sinh, gã điều khiển trình duyệt
         //    nó luôn đc phát hành song song với phiên bản trình duyệt đời thực mà bá tánh xài
         //    tương thích version với trình duyệt đang xài !important BẮT BUỘC
@@ -32,13 +36,15 @@ public class Selenium2Go {
 
         // 3. new Trình duyệt (mở trình duyệt) gắn kết với người em song sinh
         System.setProperty("webdriver.edge.driver", driverPath);
-
         // --> Báo với máy ảo Java rằng, có 1 thằng .exe muốn tham gia vào danh sách class mà JVM quản lí
         // .exe .dll xả 1 đống class điều khiển trình duyệt vào trong JVM lúc run-time
         // và gọi nhóm class này là webdriver.edge.driver -> Hằng số quy ước sẵn rồi
         // code selenium qua class WebDriver, Chrome Driver sẽ xài ở dưới đây
         // biết cách chơi với các class của .exe vừa rồi vì chúng đang cùng ở trong máy ảo.
         myBrowser = new EdgeDriver();
+        
+        
+        
 
         // 4. Viết code dùng Selenium để nói chuyện với người song sinh (driver)
         //    - Driver biết cách nch với Trình duyệt real đang đc new !!!
@@ -49,8 +55,15 @@ public class Selenium2Go {
         myBrowser.get("http://google.com"); //search web của trình duyệt vừa new
         myBrowser.manage().window().maximize();
         
+        WebElement searchBox = myBrowser.findElement(By.name("q"));
+        //đã lấy đc ô search dưới dạng object
+        searchBox.sendKeys("selenium");
+        searchBox.submit();
+        
         
         // ta sẽ đi tìm các tag, và hành xử trên các thẻ
+        
+        Thread.sleep(3000);// app nghỉ 3s rồi chạy tiếp lệnh sau
 
         //MEMORY LEAK
     }
@@ -70,7 +83,8 @@ public class Selenium2Go {
         myBrowser = new EdgeDriver(opt);
 
         myBrowser.get("http://google.com"); //search web của trình duyệt vừa new
-        myBrowser.manage().window().maximize();
+        myBrowser.manage().window().maximize();       
+        
 
     }
 }
